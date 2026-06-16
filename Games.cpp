31cca0663ca5ba0c49cc52cbd8699cef07c95fa5
kstillwell1 <stillwell.kevin@icloud.com>
2026-06-15 22:50:29 -0400

    Update Games.cpp

--- Games.cpp
+++ Games.cpp
@@ -164,10 +164,16 @@ public:
 
 	bool checkWin() override
 	{
-		static const int winLines[8][3] = {
-			{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
-			{0, 3, 6}, {1, 4, 7}, {2, 5, 8},
-			{0, 4, 8}, {2, 4, 6}
+		static const int winLines[8][3] = 
+		{
+			{0, 1, 2}, 
+			{3, 4, 5}, 
+			{6, 7, 8},
+			{0, 3, 6}, 
+			{1, 4, 7}, 
+			{2, 5, 8},
+			{0, 4, 8}, 
+			{2, 4, 6}
 		};
 
 		for (auto& winnerlines : winLines)
